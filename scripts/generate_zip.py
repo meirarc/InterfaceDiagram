aeef7bb7600464258bd57519ab119a3e342eaa41
Rafael Cabrera <meirarc@gmail.com>
2023-08-18 14:35:16 +0200

    update zip files

--- scripts/generate_zip.py
+++ scripts/generate_zip.py
@@ -1,48 +1,54 @@
-"""
-This module zip the main files in the function.zip
-The file need to be upload to a S3 to create the lambda function
-"""
+
 import zipfile
+import os
+import site
 
+# Function to log the paths that are being added to the zip file
+
+
+def log_file_paths(zipf, source_path, target_path):
+    print(
+        f"Adding file from {source_path} to {target_path} in the zip file {zipf.filename}")
+
+
+# Creating function_api.zip
 with zipfile.ZipFile('./scripts/function_api.zip', 'a') as zipf:
+    files_to_zip = [
+        ('src/main/lambda_api_function.py', 'src/main/lambda_api_function.py'),
+        ('src/main/interface_diagram.py', 'src/main/interface_diagram.py'),
+        ('src/main/encoding_helper.py', 'src/main/encoding_helper.py'),
+        ('src/main/json_parser.py', 'src/main/json_parser.py'),
+        ('src/main/config.py', 'src/main/config.py')
+    ]
 
-    zipf.write('src/main/lambda_api_function.py',
-               'src/main/lambda_api_function.py')
+    for src, dest in files_to_zip:
+        log_file_paths(zipf, src, dest)
+        zipf.write(src, dest)
 
-    zipf.write('src/main/interface_diagram.py',
-               'src/main/interface_diagram.py')
-
-    zipf.write('src/main/encoding_helper.py',
-               'src/main/encoding_helper.py')
-
-    zipf.write('src/main/json_parser.py',
-               'src/main/json_parser.py')
-
-    zipf.write('src/main/config.py',
-               'src/main/config.py')
-
-
+# Creating function_s3.zip
 with zipfile.ZipFile('./scripts/function_s3.zip', 'a') as zipf:
+    files_to_zip = [
+        ('src/main/lambda_s3_function.py', 'src/main/lambda_s3_function.py'),
+        ('src/main/interface_diagram.py', 'src/main/interface_diagram.py'),
+        ('src/main/encoding_helper.py', 'src/main/encoding_helper.py'),
+        ('src/main/json_parser.py', 'src/main/json_parser.py'),
+        ('src/main/config.py', 'src/main/config.py'),
+        ('src/main/s3_interface_url_getter.py',
+         'src/main/s3_interface_url_getter.py')
+    ]
 
-    zipf.write('src/main/lambda_s3_function.py',
-               'src/main/lambda_s3_function.py')
+    for src, dest in files_to_zip:
+        log_file_paths(zipf, src, dest)
+        zipf.write(src, dest)
 
-    zipf.write('src/main/interface_diagram.py',
-               'src/main/interface_diagram.py')
+    # Get the global site-packages directory path
+    site_packages_path = site.getsitepackages()[0]
 
-    zipf.write('src/main/encoding_helper.py',
-               'src/main/encoding_helper.py')
-
-    zipf.write('src/main/json_parser.py',
-               'src/main/json_parser.py')
-
-    zipf.write('src/main/config.py',
-               'src/main/config.py')
-
-    zipf.write('src/main/s3_interface_url_getter.py',
-               'src/main/s3_interface_url_getter.py')
-    
-    import os
-    for root, dirs, files in os.walk('./.venv/Lib/site-packages'):
+    # Package the libraries from the global site-packages directory
+    for root, dirs, files in os.walk(site_packages_path):
         for file in files:
-            zipf.write(os.path.join(root, file), os.path.join(root.replace('./.venv/Lib/site-packages', 'package'), file))
+            src_path = os.path.join(root, file)
+            dest_path = os.path.join(root.replace(
+                site_packages_path, 'package'), file)
+            log_file_paths(zipf, src_path, dest_path)
+            zipf.write(src_path, dest_path)
