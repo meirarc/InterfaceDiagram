c480b96c600ae71ac315bc3f7b3677a22b676a8f
Rafael Cabrera <meirarc@gmail.com>
2023-08-18 16:40:18 +0200

    parse

--- src/main/s3_interface_url_getter.py
+++ src/main/s3_interface_url_getter.py
@@ -105,7 +105,7 @@ class S3InterfaceURLGetter:
         bucket, key = path.split('/', 1)
 
         print('_parse_s3_path', bucket, key)
-        return bucket, key.lstrip('in/')
+        return bucket, key.lstrip('/')
 
     def process_json_files(self):
         """
