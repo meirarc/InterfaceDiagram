45b683a2398c676674a5a0bdf1f8b2587afd6f92
Rafael Cabrera <meirarc@gmail.com>
2023-08-18 21:52:32 +0200

    _move_file error

--- src/main/s3_interface_url_getter.py
+++ src/main/s3_interface_url_getter.py
@@ -43,8 +43,6 @@ class S3InterfaceURLGetter:
             'error_file_path': ''
         }
 
-        print('__init__', self.file_info)
-
         self.parser = JSONParser()
         self.encoder = EncodingHelper()
         self.interfaces = None
@@ -56,64 +54,50 @@ class S3InterfaceURLGetter:
             self.s3_client = boto3.client('s3')
 
     def _list_files(self, directory):
-        print('_list_files', directory)
-        if self.file_info['is_s3']:
-            bucket, prefix = self._parse_s3_path(directory)
-            result = self.s3_client.list_objects(Bucket=bucket, Prefix=prefix)
-            for content in result.get('Contents', []):
-                yield content['Key']
+        bucket, prefix = self._parse_s3_path(directory)
+        result = self.s3_client.list_objects(Bucket=bucket, Prefix=prefix)
+        for content in result.get('Contents', []):
+            yield content['Key']
 
     def _read_json(self, filepath):
-        print('_read_json', filepath)
-        if self.file_info['is_s3']:
-            bucket, key = self._parse_s3_path(filepath)
-            response = self.s3_client.get_object(Bucket=bucket, Key=key)
-
-            json_content = response['Body'].read().decode('utf-8')
-
-            df = pd.read_json(json_content)
-
-            print('_read_json:response: ', response)
-            print('_read_json:json_content: ', json_content)
-            print('_read_json:df: ', df)
-            # return [f.lstrip('in/') for f in pd.read_json(response['Body'])]
-            return df.to_dict(orient='records')
-        return None
+        bucket, key = self._parse_s3_path(filepath)
+        response = self.s3_client.get_object(Bucket=bucket, Key=key)
+        json_content = response['Body'].read().decode('utf-8')
+        data_frame = pd.read_json(json_content)
+        return data_frame.to_dict(orient='records')  # pylint: disable=E1101
 
     def _save_to_excel(self, data_frame, filepath):
-        print('_save_to_excel', data_frame, filepath)
-        if self.file_info['is_s3']:
-            excel_buffer = io.BytesIO()
-            # Save the DataFrame to the BytesIO object as an Excel file
-            data_frame.to_excel(excel_buffer, index=False, engine='openpyxl')
-
-            # Reset the buffer's position to the beginning
-            excel_buffer.seek(0)
-
-            # Extract the bucket and key from the S3 filepath
-            bucket, key = self._parse_s3_path(filepath)
-
-            # Upload the buffer contents (the Excel file) to S3
-            self.s3_client.put_object(
-                Bucket=bucket, Key=key, Body=excel_buffer
-            )
+        excel_buffer = io.BytesIO()
+        # Save the DataFrame to the BytesIO object as an Excel file
+        data_frame.to_excel(excel_buffer, index=False, engine='openpyxl')
+        # Reset the buffer's position to the beginning
+        excel_buffer.seek(0)
+        # Extract the bucket and key from the S3 filepath
+        bucket, key = self._parse_s3_path(filepath)
+        # Upload the buffer contents (the Excel file) to S3
+        self.s3_client.put_object(
+            Bucket=bucket, Key=key, Body=excel_buffer
+        )
 
     def _move_file(self, src_path, dest_path):
-        print('_move_file', src_path, dest_path)
-        if self.file_info['is_s3']:
-            src_bucket, src_key = self._parse_s3_path(src_path)
-            dest_bucket, dest_key = self._parse_s3_path(dest_path)
-            self.s3_client.copy_object(Bucket=dest_bucket, CopySource={
-                                       'Bucket': src_bucket, 'Key': src_key}, Key=dest_key)
-            self.s3_client.delete_object(Bucket=src_bucket, Key=src_key)
+        print(f'_move_file: src_path ({src_path}), dest_path ({dest_path})')
+
+        src_bucket, src_key = self._parse_s3_path(src_path)
+        dest_bucket, dest_key = self._parse_s3_path(dest_path)
+
+        self.s3_client.copy_object(Bucket=dest_bucket, CopySource={
+            'Bucket': src_bucket, 'Key': src_key}, Key=dest_key)
+        self.s3_client.delete_object(Bucket=src_bucket, Key=src_key)
 
     def _parse_s3_path(self, path):
-        print('_parse_s3_path', path)
+        print('_parse_s3_path: path:', path)
+
         assert path.startswith('s3://')
         path = path[5:]
         bucket, key = path.split('/', 1)
 
-        print('_parse_s3_path', bucket, key)
+        print(f'_parse_s3_path: bucket ({bucket}), key ({key})')
+
         return bucket, key.lstrip('/')
 
     def process_json_files(self):
@@ -123,6 +107,11 @@ class S3InterfaceURLGetter:
         If no JSON files are found in the source directory, a new DataFrame with one
         empty row is created to represent a blank record.
         """
+        if not self.file_info['is_s3']:
+            print(
+                f'process_json_files: self.file_info["is_s3"]: {self.file_info["is_s3"]}')
+            return
+
         # Initialize a variable to track whether any JSON files are found
         file_control = {
             'json_files_found': False,
@@ -133,28 +122,28 @@ class S3InterfaceURLGetter:
         for filename in self._list_files(self.file_info['source_dir']):
             if filename.endswith('.json'):
 
-                file_name = filename.lstrip('in/')
+                clean_file_name = filename.lstrip('in/')
 
-                print('process_json_files', file_name)
+                print(
+                    f'process_json_files: clean_file_name: ({clean_file_name})')
 
                 # Set the flag to True when a JSON file is found
                 file_control['json_files_found'] = True
 
                 # Path to the source file and its potential backup
                 self.file_info['source_path'] = os.path.join(
-                    self.file_info['source_dir'], file_name)
+                    self.file_info['source_dir'], clean_file_name)
                 self.file_info['backup_path'] = os.path.join(
-                    self.file_info['backup_dir'], file_name)
+                    self.file_info['backup_dir'], clean_file_name)
+
+                print(
+                    f'process_json_files: self.file_info["source_path"]: ({self.file_info["source_path"]})')
+                print(
+                    f'process_json_files: self.file_info["backup_path"]: ({self.file_info["backup_path"]})')
 
                 # Check if the backup file exists and compare the contents
                 # Flag to decide whether to process the file or not
                 file_control['process_file'] = True
-
-                print('process_json_files', file_control)
-
-                print('process_json_files', self.file_info)
-                print('process_json_files', os.path.exists(
-                    self.file_info['backup_path']))
 
                 if os.path.exists(self.file_info['backup_path']):
 
@@ -174,12 +163,9 @@ class S3InterfaceURLGetter:
 
                 # If there's no backup or the contents are different, process the source file
 
-                print('process_json_files', file_control['process_file'])
                 if file_control['process_file']:
 
                     data = self._read_json(self.file_info['source_path'])
-                    # with open(self.file_info['source_path'], 'r', encoding='utf-8') as file:
-                    #    data = json.load(file)
 
                     # Initialize app_name to None at the beginning of each iteration
                     app_name = None
@@ -210,25 +196,20 @@ class S3InterfaceURLGetter:
                                             'connected_app'] = app_name
                         self.data_frame.loc[new_index,
                                             'body'] = self.parser.dumps(data)
-                        self.data_frame.loc[new_index, 'file_name'] = file_name
+                        self.data_frame.loc[new_index,
+                                            'file_name'] = clean_file_name
                         self.data_frame.loc[new_index, 'url'] = url
 
                         # Move the successfully processed file to backup
-                        # shutil.move(
-                        #    self.file_info['source_path'], self.file_info['backup_path'])
-
                         self._move_file(
                             self.file_info['source_path'], self.file_info['backup_path'])
 
                     except KeyError as key_error:
-                        print(f'Error processing file {file_name}.'
+                        print(f'Error processing file {clean_file_name}.'
                               f' Skipping. KeyError: {key_error}')
 
                         self.file_info['error_file_path'] = os.path.join(
-                            self.file_info['error_dir'], file_name)
-
-                        # shutil.move(self.file_info['source_path'],
-                        #            self.file_info['error_file_path'])
+                            self.file_info['error_dir'], clean_file_name)
 
                         self._move_file(
                             self.file_info['source_path'], self.file_info['error_file_path'])
@@ -243,4 +224,10 @@ class S3InterfaceURLGetter:
         """
         Saves the new records (stored in self.df) to the specified sheet in the Excel file.
         """
+
+        if not self.file_info['is_s3']:
+            print(
+                f'save_results: self.file_info["is_s3"]: {self.file_info["is_s3"]}')
+            return
+
         self._save_to_excel(self.data_frame, self.file_info['excel_file'])
