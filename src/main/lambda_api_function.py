49b71930aa371fd39bc742122127bcc5a3dbf1d4
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 22:00:38 +0200

    adjusted documentation for lambda api

--- src/main/lambda_api_function.py
+++ src/main/lambda_api_function.py
@@ -1,28 +1,42 @@
 """
-Lamda Function: InterdaceDiagram
+AWS Lambda Function: InterfaceDiagram
+
+This Lambda function handles the generation of an interface diagram URL based on
+the input JSON data.
 """
 from src.main.interface_diagram import InterfaceDiagram
 from src.main.json_parser import JSONParser
 from src.main.encoding_helper import EncodingHelper
 
+
 def lambda_handler(event, _):
     """
-    Lambda Handler function
-    """
-    # Convert the data string into a dictionary
-    parser = JSONParser()
-    data = parser.parse(event['body'])
+    AWS Lambda Handler function to generate a diagram URL.
 
-    interfaces = parser.json_to_object(data)
+    :param event: AWS Lambda event object containing the request details.
+    :param _: Context parameter (unused).
+    :return: Dictionary containing the response, including the diagram URL.
+    """
+
+    # Initialize JSON parser and EncodingHelper
+    parser = JSONParser()
     encoder = EncodingHelper()
 
-    diagram = InterfaceDiagram(interfaces, encoder)  # Initialize the InterfaceDiagram with the data
+    # Parse the JSON data from the event body
+    data = parser.parse(event['body'])
+
+    # Convert the parsed data to the required object format
+    interfaces = parser.json_to_object(data)
+
+    # Initialize the InterfaceDiagram class and generate the diagram URL
+    diagram = InterfaceDiagram(interfaces, encoder)
     url = diagram.generate_diagram_url()
 
+    # Prepare the Lambda function response
     result = {
         "isBase64Encoded": False,
         "statusCode": 200,
-        "headers": { 'Content-Type': 'application/json' },
+        "headers": {'Content-Type': 'application/json'},
         "body": url
     }
 
