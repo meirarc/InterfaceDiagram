177057141b5dae15f0e38d398d26c7fe2d477a73
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 21:36:28 +0200

    refactor upoad json to s3 script

--- scripts/upload_json_to_s3.py
+++ scripts/upload_json_to_s3.py
@@ -1,28 +1,57 @@
+"""
+This script uploads JSON files from a local directory to an AWS S3 bucket.
+After uploading, it moves the files to a local backup directory.
+"""
 import os
 import shutil
+from typing import List
+
 import boto3
 
 # Set AWS S3 Configurations
-s3_bucket = 'interface-diagram-files'
-s3_in_dir = 'in/'
+S3_BUCKET = 'interface-diagram-files'
+S3_IN_DIR = 'in/'
 
 # Set local directories
-local_in_dir = './diagram/in/'
-local_backup_dir = './diagram/in/backup/'
+LOCAL_IN_DIR = './diagram/in/'
+LOCAL_BACKUP_DIR = './diagram/in/backup/'
 
-# Initialize boto3 S3 client
-s3_client = boto3.client('s3')
 
-# List all files in local 'in' directory
-for filename in os.listdir(local_in_dir):
-    if filename.endswith('.json'):
-        # Full path to the file
-        filepath = os.path.join(local_in_dir, filename)
+def upload_and_move_files(filenames: List[str], s3_client: boto3.client) -> None:
+    """
+    Upload files to S3 and move them to a local backup directory.
 
-        # Upload file to S3
-        s3_client.upload_file(filepath, s3_bucket, s3_in_dir + filename)
+    Args:
+    filenames (List[str]): List of filenames to be uploaded and moved.
+    s3_client (boto3.client): Initialized boto3 S3 client.
+    """
+    for filename in filenames:
+        if filename.endswith('.json'):
+            # Full path to the file
+            filepath = os.path.join(LOCAL_IN_DIR, filename)
 
-        # Move the uploaded file to the backup folder
-        shutil.move(filepath, os.path.join(local_backup_dir, filename))
+            # Upload file to S3
+            s3_client.upload_file(filepath, S3_BUCKET,
+                                  f"{S3_IN_DIR}{filename}")
 
-        print(f"Uploaded and moved {filename} to backup.")
+            # Move the uploaded file to the backup folder
+            shutil.move(filepath, os.path.join(LOCAL_BACKUP_DIR, filename))
+
+            print(f"Uploaded and moved {filename} to backup.")
+
+
+def main() -> None:
+    """
+    Main function to upload and move files.
+    """
+    # Initialize boto3 S3 client
+    s3_client = boto3.client('s3')
+
+    # List all files in local 'in' directory
+    filenames = os.listdir(LOCAL_IN_DIR)
+
+    upload_and_move_files(filenames, s3_client)
+
+
+if __name__ == '__main__':
+    main()
