9cadb6bc9aaa70a93ba0d17d54b058fb9c5ea252
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 22:19:54 +0200

    refactor local url getter

--- src/main/local_interface_url_getter.py
+++ src/main/local_interface_url_getter.py
@@ -5,13 +5,13 @@ This module contains a class that reads JSON files from a specified directory,
 processes the contents of these files, and updates an Excel file with the parsed data.
 If no JSON files are found, a blank record is created in the Excel file.
 """
-import json
 import os
-
+import json
 import shutil
+from typing import Dict
+import pandas as pd
 from openpyxl.worksheet.table import Table, TableStyleInfo
 from openpyxl import load_workbook
-import pandas as pd
 
 from src.main.encoding_helper import EncodingHelper
 from src.main.json_parser import JSONParser
@@ -29,10 +29,8 @@ class LocalInterfaceURLGetter:
 
     def __init__(self, source_dir, excel_file):
         """
-        Initializes the DiagramURLUpdater with specified directory paths and an empty DataFrame.
+        Initializes the LocalInterfaceURLGetter with specified directory paths and an empty DataFrame.
         """
-
-        # Specify directories
 
         self.file_info = {
             'source_dir': source_dir,
@@ -54,130 +52,97 @@ class LocalInterfaceURLGetter:
     def process_json_files(self):
         """
         Processes JSON files from the source directory and updates the DataFrame.
-
-        If no JSON files are found in the source directory, a new DataFrame with one
-        empty row is created to represent a blank record.
         """
-        # Initialize a variable to track whether any JSON files are found
-        file_control = {
-            'json_files_found': False,
-            'process_file': True
-        }
 
-        file_control['json_files_found'] = False
+        file_control = {'json_files_found': False}
 
-        # Loop through files in directory
+        # Process each file in the source directory
         for filename in os.listdir(self.file_info['source_dir']):
             if filename.endswith('.json'):
-
-                # Set the flag to True when a JSON file is found
                 file_control['json_files_found'] = True
-
-                # Path to the source file and its potential backup
-                self.file_info['source_path'] = os.path.join(
-                    self.file_info['source_dir'], filename)
-                self.file_info['backup_path'] = os.path.join(
-                    self.file_info['backup_dir'], filename)
-
-                # Check if the backup file exists and compare the contents
-                # Flag to decide whether to process the file or not
-                file_control['process_file'] = True
-
-                if os.path.exists(self.file_info['backup_path']):
-
-                    with open(self.file_info['source_path'], 'r',
-                              encoding='utf-8') as source_file, \
-                            open(self.file_info['backup_path'], 'r',
-                                 encoding='utf-8') as backup_file:
-
-                        # Load content of both files
-                        source_content = json.load(source_file)
-                        backup_content = json.load(backup_file)
-
-                    # If content is identical, don't process the file
-                    if source_content == backup_content:
-                        file_control['process_file'] = False
-                        os.remove(self.file_info['source_path'])
-
-                # If there's no backup or the contents are different, process the source file
-                if file_control['process_file']:
-                    with open(self.file_info['source_path'], 'r', encoding='utf-8') as file:
-                        data = json.load(file)
-
-                    # Initialize app_name to None at the beginning of each iteration
-                    app_name = None
-
-                    # Extract app_name for app_type as 'connected_app'
-                    for item in data:
-                        if item['app_type'] == 'connected_app':
-                            app_name = item['app_name']
-                            break
-
-                    # If app_name is not found, set it to filename
-                    if app_name is None:
-                        app_name = ''
-
-                    # Get the Draw.io URL of the diagram and save it to a file
-                    try:
-                        self.interfaces = self.parser.json_to_object(data)
-
-                        # Initialize the InterfaceDiagram with the data
-                        diagram = InterfaceDiagram(
-                            self.interfaces, self.encoder)
-
-                        url = diagram.generate_diagram_url()  # Generate the diagram
-
-                        # Add the data to the DataFrame
-                        new_index = len(self.data_frame)
-                        self.data_frame.loc[new_index,
-                                            'connected_app'] = app_name
-                        self.data_frame.loc[new_index,
-                                            'body'] = self.parser.dumps(data)
-                        self.data_frame.loc[new_index, 'file_name'] = filename
-                        self.data_frame.loc[new_index, 'url'] = url
-
-                        # Move the successfully processed file to backup
-                        shutil.move(
-                            self.file_info['source_path'], self.file_info['backup_path'])
-
-                    except KeyError as key_error:
-                        print(f'Error processing file {filename}.'
-                              f' Skipping. KeyError: {key_error}')
-
-                        self.file_info['error_file_path'] = os.path.join(
-                            self.file_info['error_dir'], filename)
-
-                        shutil.move(self.file_info['source_path'],
-                                    self.file_info['error_file_path'])
-                        continue
+                self.process_single_file(filename)
 
         if not file_control['json_files_found']:
             # Create a new DataFrame with one empty row
             self.data_frame = pd.DataFrame(
-                columns=['connected_app', 'body', 'file_name', 'url'], index=[0])
+                columns=['connected_app', 'body', 'file_name', 'url'], index=[0]
+            )
+
+    def process_single_file(self, filename: str):
+        """
+        Processes a single JSON file.
+        """
+        source_path = os.path.join(self.file_info['source_dir'], filename)
+        backup_path = os.path.join(self.file_info['backup_dir'], filename)
+
+        if os.path.exists(backup_path):
+            if self.is_content_identical(source_path, backup_path):
+                os.remove(source_path)
+                return
+
+        try:
+            with open(source_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+
+            app_name = self.get_connected_app_name(data)
+            self.interfaces = self.parser.json_to_object(data)
+
+            diagram = InterfaceDiagram(self.interfaces, self.encoder)
+            url = diagram.generate_diagram_url()
+
+            self.append_to_data_frame(app_name, data, filename, url)
+            shutil.move(source_path, backup_path)
+
+        except KeyError as key_error:
+            print(
+                f'Error processing file {filename}. Skipping. KeyError: {key_error}')
+
+            error_file_path = os.path.join(
+                self.file_info['error_dir'], filename)
+            shutil.move(source_path, error_file_path)
+
+    def is_content_identical(self, source_path: str, backup_path: str) -> bool:
+        """
+        Checks if the content of the source file is identical to its backup.
+        """
+        with open(source_path, 'r', encoding='utf-8') as source_file, \
+                open(backup_path, 'r', encoding='utf-8') as backup_file:
+            source_content = json.load(source_file)
+            backup_content = json.load(backup_file)
+        return source_content == backup_content
+
+    def get_connected_app_name(self, data: Dict) -> str:
+        """
+        Extracts the connected app name from the data.
+        """
+        for item in data:
+            if item['app_type'] == 'connected_app':
+                return item['app_name']
+        return ''
+
+    def append_to_data_frame(self, app_name: str, data: Dict, filename: str, url: str):
+        """
+        Appends a new row to the DataFrame.
+        """
+        new_index = len(self.data_frame)
+        self.data_frame.loc[new_index] = [
+            app_name, self.parser.dumps(data), filename, url]
 
     def save_results(self):
         """
         Saves the new records (stored in self.df) to the specified sheet in the Excel file.
         """
-        # Save the processed data back to the Excel file
         self.data_frame.to_excel(
             self.file_info['excel_file'], index=False, engine='openpyxl')
 
-        # Open the Excel file with openpyxl
         work_book = load_workbook(self.file_info['excel_file'])
         work_space = work_book.active
 
-        # Define a table and add it to the worksheet
         tab = Table(displayName="Table1", ref=work_space.dimensions)
-
-        # Add a default style to the table
         style = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
                                showLastColumn=False, showRowStripes=True, showColumnStripes=False)
+
         tab.tableStyleInfo = style
-
         work_space.add_table(tab)
-
-        # Save the Excel file with the table
         work_book.save(self.file_info['excel_file'])
         work_book.close()
