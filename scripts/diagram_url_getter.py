44cef37a2eddc04715ed1e6cd45b81da51c65b48
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 21:12:36 +0200

    adjust documentation on the local script

--- scripts/diagram_url_getter.py
+++ scripts/diagram_url_getter.py
@@ -1,9 +1,11 @@
 """
-Module to update the Interface Diagram URL Excel file.
+Module to process and update the Interface Diagram URL Excel file.
 
-This module contains a class that reads JSON files from a specified directory,
+This module contains a script that reads JSON files from a specified directory,
 processes the contents of these files, and updates an Excel file with the parsed data.
 If no JSON files are found, a blank record is created in the Excel file.
+
+Usage: setup a scheduler to run the E2E Flow diagram local or on a server
 """
 from src.main.local_interface_url_getter import LocalInterfaceURLGetter
 
