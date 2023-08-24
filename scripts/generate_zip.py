de2788cab627ca49cfb9dce3aa8e24174ff96ac6
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 16:34:15 +0200

    create new lambda and layer to test on AWS

--- scripts/generate_zip.py
+++ scripts/generate_zip.py
@@ -11,7 +11,7 @@ def log_file_paths(zipf, source_path, target_path):
 
 
 # Creating function_api.zip
-with zipfile.ZipFile('./scripts/function_api.zip', 'a') as zipf:
+with zipfile.ZipFile('./scripts/lambda_api_function.zip', 'a') as zipf:
     files_to_zip = [
         ('src/main/lambda_api_function.py', 'src/main/lambda_api_function.py'),
         ('src/main/interface_diagram.py', 'src/main/interface_diagram.py'),
@@ -25,7 +25,7 @@ with zipfile.ZipFile('./scripts/function_api.zip', 'a') as zipf:
         zipf.write(src, dest)
 
 # Creating function_s3.zip
-with zipfile.ZipFile('./scripts/function_s3.zip', 'a') as zipf:
+with zipfile.ZipFile('./scripts/lambda_s3_function.zip', 'a') as zipf:
     files_to_zip = [
         ('src/main/lambda_s3_function.py', 'src/main/lambda_s3_function.py'),
         ('src/main/interface_diagram.py', 'src/main/interface_diagram.py'),
@@ -40,6 +40,38 @@ with zipfile.ZipFile('./scripts/function_s3.zip', 'a') as zipf:
         log_file_paths(zipf, src, dest)
         zipf.write(src, dest)
 
+    # Get the global site-packages directory path
+    site_packages_path = site.getsitepackages()[0]
+
+    # Package the libraries from the global site-packages directory
+    # directly at the root level of the zip file
+    for root, dirs, files in os.walk(site_packages_path):
+        for file in files:
+            src_path = os.path.join(root, file)
+            dest_path = os.path.relpath(
+                os.path.join(root, file), site_packages_path)
+            log_file_paths(zipf, src_path, dest_path)
+            zipf.write(src_path, dest_path)
+
+# Creating function_s3.zip
+with zipfile.ZipFile('./scripts/lambda_s3_function_no_package.zip', 'a') as zipf:
+    files_to_zip = [
+        ('src/main/lambda_s3_function.py', 'src/main/lambda_s3_function.py'),
+        ('src/main/interface_diagram.py', 'src/main/interface_diagram.py'),
+        ('src/main/encoding_helper.py', 'src/main/encoding_helper.py'),
+        ('src/main/json_parser.py', 'src/main/json_parser.py'),
+        ('src/main/config.py', 'src/main/config.py'),
+        ('src/main/s3_interface_url_getter.py',
+         'src/main/s3_interface_url_getter.py')
+    ]
+
+    for src, dest in files_to_zip:
+        log_file_paths(zipf, src, dest)
+        zipf.write(src, dest)
+
+
+# Creating function_s3.zip
+with zipfile.ZipFile('./scripts/lambda_s3_layer.zip', 'a') as zipf:
     # Get the global site-packages directory path
     site_packages_path = site.getsitepackages()[0]
 
