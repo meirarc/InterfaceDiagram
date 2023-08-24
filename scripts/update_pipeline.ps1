3c3f303999c81bce3875308e0ec3451c7081b0b9
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 21:09:10 +0200

    update local pipeline.ps1 with new zip files

--- scripts/update_pipeline.ps1
+++ scripts/update_pipeline.ps1
@@ -6,26 +6,27 @@ aws cloudformation delete-stack --stack-name diagram
 Write-Host "Removing local function.zip..."
 Remove-Item -Path .\scripts\function.zip
 
+Remove-Item -Path .\scripts\lambda_api_function.zip
+#Remove-Item -Path .\scripts\lambda_s3_function.zip
+Remove-Item -Path .\scripts\lambda_s3_function_no_package.zip
+Remove-Item -Path .\scripts\lambda_s3_layer.zip
+
 # 3. Execute the Python script to generate the function.zip
 Write-Host "Generating function.zip..."
 python .\scripts\generate_zip.py
 
 # 4. Upload function.zip and lambda_function.yaml to S3
 Write-Host "Uploading files to S3..."
-aws s3 cp .\scripts\function.zip s3://interface-diagram/function.zip
-aws s3 cp .\scripts\lambda_function.yaml s3://interface-diagram/lambda_function.yaml
+aws s3 cp ./scripts/lambda_api_function.zip s3://interface-diagram/lambda_api_function.zip
+# aws s3 cp ./scripts/lambda_s3_function.zip s3://interface-diagram/lambda_s3_function.zip
+aws s3 cp ./scripts/lambda_s3_function_no_package.zip s3://interface-diagram/lambda_s3_function_no_package.zip
+aws s3 cp ./scripts/lambda_s3_layer.zip s3://interface-diagram/lambda_s3_layer.zip
+aws s3 cp ./scripts/lambda_function.yaml s3://interface-diagram/lambda_function.yaml
 
 # 5. Wait for the stack to be completely deleted
 Write-Host "Waiting for stack deletion..."
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 80
 
 # 6. Create the new stack.
 Write-Host "Creating new stack..."
 aws cloudformation create-stack --stack-name diagram --template-url https://interface-diagram.s3.amazonaws.com/lambda_function.yaml
-
-# 7. Retrieve the stack output
-Write-Host "Waiting for stack creation..."
-Start-Sleep -Seconds 20  # Adjust this if you want to wait longer
-
-$stackOutput = aws cloudformation describe-stacks --stack-name diagram --query "Stacks[0].Outputs"
-Write-Host $stackOutput
