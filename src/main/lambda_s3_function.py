bed7cef63595cb189854c792d5ae56657f86bed5
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 22:03:40 +0200

    adjust documention for lambda s3

--- src/main/lambda_s3_function.py
+++ src/main/lambda_s3_function.py
@@ -1,27 +1,38 @@
 """
-Lambda Function
+AWS Lambda Function for Interface URL Getting
+
+This Lambda function processes JSON files from an S3 bucket, generates interface diagram URLs,
+and saves the results in an Excel file.
 """
 
 from src.main.s3_interface_url_getter import S3InterfaceURLGetter
-import sys
 import json
 
 
 def lambda_handler(event, _):
     """
-    Lambda Function
+    AWS Lambda Handler function to process JSON files and save results.
+
+    :param event: AWS Lambda event object containing the request details.
+    :param _: Context parameter (unused).
+    :return: Dictionary containing the response.
     """
-    # Print the event object to CloudWatch Logs
+
+    # Log the event object to CloudWatch Logs
     print("Received event: " + json.dumps(event, indent=2))
 
+    # Define source directory and Excel file paths
     source_dir = 's3://interface-diagram-files/in/'
     excel_file = 's3://interface-diagram-files/out/interfaces_diagrams_urls.xlsx'
 
+    # Initialize the S3InterfaceURLGetter class
     getter = S3InterfaceURLGetter(source_dir, excel_file)
 
+    # Process the JSON files and save the results
     getter.process_json_files()
     getter.save_results()
 
+    # Prepare the Lambda function response
     return {
         'statusCode': 200,
         'body': json.dumps('Successfully processed files.')
