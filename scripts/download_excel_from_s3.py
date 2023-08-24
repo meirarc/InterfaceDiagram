3a69b80270a307a8aca56bd59648d53833cc6943
Rafael Cabrera <meirarc@gmail.com>
2023-08-24 21:18:54 +0200

    adjusted documentation to the download s3 script

--- scripts/download_excel_from_s3.py
+++ scripts/download_excel_from_s3.py
@@ -1,19 +1,30 @@
+"""
+This script downloads an Excel file containing interface diagrams URLs
+from an AWS S3 bucket to a local directory.
+"""
 import os
 import boto3
 
 # Set AWS S3 Configurations
-s3_bucket = 'interface-diagram-files'
-s3_out_file_key = 'out/interfaces_diagrams_urls.xlsx'
+S3_BUCKET = 'interface-diagram-files'
+S3_OUT_FILE_KEY = 'out/interfaces_diagrams_urls.xlsx'
 
 # Set local directories
-local_out_dir = './diagram/out/'
-local_out_filepath = os.path.join(
-    local_out_dir, 'interfaces_diagrams_urls.xlsx')
+LOCAL_OUT_DIR = './diagram/out/'
+LOCAL_OUT_FILEPATH = os.path.join(
+    LOCAL_OUT_DIR, 'interfaces_diagrams_urls.xlsx')
 
-# Initialize boto3 S3 client
-s3_client = boto3.client('s3')
 
-# Download the Excel file from S3 to local 'out' directory
-s3_client.download_file(s3_bucket, s3_out_file_key, local_out_filepath)
+def main():
+    """Main function to download the file from S3."""
+    # Initialize boto3 S3 client
+    s3_client = boto3.client('s3')
 
-print(f"Downloaded {local_out_filepath} from S3.")
+    # Download the Excel file from S3 to local 'out' directory
+    s3_client.download_file(S3_BUCKET, S3_OUT_FILE_KEY, LOCAL_OUT_FILEPATH)
+
+    print(f"Downloaded {LOCAL_OUT_FILEPATH} from S3.")
+
+
+if __name__ == '__main__':
+    main()
